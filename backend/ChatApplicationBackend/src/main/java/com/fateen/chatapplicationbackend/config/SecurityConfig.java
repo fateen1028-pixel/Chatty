3af9a6ac4bca6f37e7f637c40051a1797a94ada0
Mohamed Fateen F <fateen1028@gmail.com>
2026-05-22 10:01:40 +0530

    Wiring the frontend URL , in CORS

--- backend/ChatApplicationBackend/src/main/java/com/fateen/chatapplicationbackend/config/SecurityConfig.java
+++ backend/ChatApplicationBackend/src/main/java/com/fateen/chatapplicationbackend/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
 
-                    config.setAllowedOriginPatterns(List.of("http://localhost:*"));
+                    config.setAllowedOriginPatterns(List.of("http://localhost:5173","https://chatapp-frontend-1o5.pages.dev"));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
                     config.setExposedHeaders(List.of("Authorization"));
